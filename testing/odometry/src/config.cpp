9d85202c43953e29d91e26a951636fd6e99d0b76
Nikhil Gante <odyseus05@gmail.com>
2022-07-10 16:54:49 -0400

    Added odom folder

---
+++ testing/odometry/src/config.cpp
@@ -0,0 +1,15 @@
+#include "config.hpp"
+
+
+Controller master(pros::E_CONTROLLER_MASTER);
+Controller partner(pros::E_CONTROLLER_PARTNER);
+
+static constexpr int e_port = 20;
+
+pros::ADIEncoder LeftEncoder({e_port, 1, 2}, false), RightEncoder({e_port, 5, 6}, true), BackEncoder({e_port, 3, 4}, false);
+
+// x-drive motor config
+pros::Motor front_l(3, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor back_l(2, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor front_r(7, pros::E_MOTOR_GEARSET_18, true, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor back_r(1, pros::E_MOTOR_GEARSET_18, true, pros::E_MOTOR_ENCODER_DEGREES);
---
+++ testing/odometry/src/config.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include "main.h"
+
+using namespace pros;
+
+extern Controller master;
+extern Controller partner;
+extern pros::Motor front_l, front_r, back_l, back_r, flywheel_front, flywheel_back;
+extern pros::ADIEncoder LeftEncoder, RightEncoder, BackEncoder;
